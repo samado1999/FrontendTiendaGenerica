0c47856a9b7cb54d4be492dce71e1d2e0eca3c9b
Samuel Amado Ramirez <samuelamadoramirez@gmail.com>
2021-11-07 21:21:16 -0500

    Merge branch 'front.sa' of https://github.com/samado1999/FrontendTiendaGenerica into front.sa

--- app.js
+++ app.js
@@ -1,23 +1,35 @@
-async function postData() {
-    const response = await fetch('http://localhost:9090/api/productos/save', {
+async function postData(list) {
+    const response = await fetch('http://localhost:9090/api/productos/saveAll', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({
-            buyPrice: "string",
-            id: "string",
-            iva: "string",
-            productCode: "string",
-            productName: "string",
-            sellPrice: "string",
-            supplierNit: "string"
-        })
+        body: JSON.stringify(list)
     });
-    const data = await response.json();
-    console.log(data);
+    console.log('RESPONSE: ' + response.body);
 }
 
-function parseProduct() {
-    
+function cargarArchivo() {
+    var list = [];
+    var csv = fileupload.files[0];
+    var reader = new FileReader();
+
+    reader.onload = function (event) {
+        var text = event.target.result;
+        const rows = text.slice(0).split("\n");
+        for (var i = 0; i < rows.length; i++) {
+            var fields = rows[i].split(",");
+            const prod = new Object();
+            prod.productCode = String(fields[0].trim());
+            prod.productName = String(fields[1].trim());
+            prod.supplierNit = String(fields[2].trim())
+            prod.buyPrice = String(fields[3].trim());
+            prod.iva = String(fields[4].trim());
+            prod.sellPrice = String(fields[5].trim());
+            list.push(prod);
+        }
+        console.log('LIST: ' + JSON.stringify(list));
+        postData(list);
+    }
+    reader.readAsText(csv);
 }
